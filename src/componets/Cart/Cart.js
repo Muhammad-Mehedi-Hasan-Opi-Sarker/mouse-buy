7b46bb742b1ae49e96e7cac9811f4c19104d2a77
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-03-26 16:05:35 +0600

    selected items button

---
+++ src/componets/Cart/Cart.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Cart = () => {
+    return (
+        <div>
+            <h2>Selected Items</h2>
+              <div>
+              <button><h5>Choose 1 For Me</h5></button>
+              </div>
+              <div>
+                  {/* here product items */}
+              </div>
+              <div>
+              <button><h5>Choose Again</h5></button>
+              </div>
+        </div>
+    );
+};
+
+export default Cart;
